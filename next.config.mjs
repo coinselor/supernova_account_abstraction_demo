3c57a9075b1ab801df4c8d6a737dbf7a66ab4db0
alienc0der <118136375+alienc0der@users.noreply.github.com>
2024-11-27 22:20:27 +0200

    fix: update `nextConfig`

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-  };
-  
-  export default nextConfig;
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/supernova_account_abstraction_demo' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/supernova_account_abstraction_demo/' : '',
+};
+
+export default nextConfig;
